6e555512ff4d96191e063a83bd9c6b6e4f2adab4
risema <richi2001.rsm@gmail.com>
2023-06-15 15:04:22 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('yukimaga_db','userku','passku',{
+const db = new Sequelize('yukimaga_db','49157','49157',{
     host: "db-49157",
     port: 3306,    
     dialect: "mysql"
